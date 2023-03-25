352d069e2befa81273d4f5f6af7b1a14bbef6bfa
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2023-03-25 16:39:00 +0545

    Login form added

--- mern-profile/client/src/App.js
+++ mern-profile/client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 import About from './components/About'
 
-import { Route, Routes } from 'react-router-dom'
 import Signup from './components/Signup'
+import Login from './components/Login'
+import Contact from './components/Contact'
+
 const App = () => {
   return (
     <>
@@ -14,6 +17,8 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/signup' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/contact' element={<Contact />} />
 
 
 
--- mern-profile/client/src/components/Login.js
+++ mern-profile/client/src/components/Login.js
@@ -1,38 +1,31 @@
+import React from 'react'
 
-// const Navbar = () => {
-//     return (
-//         <div className='container'>
-//             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//                 <NavLink  className="navbar-brand" to="#">Navbar w/ text</NavLink>
-//                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-//                     <span className="navbar-toggler-icon"></span>
-//                 </button>
-//                 <div className="collapse navbar-collapse" id="navbarText">
-                   
-            
-                       
-//                         <ul className="navbar-nav d-flex ">
-//                         <li className="nav-item active">
-//                             <NavLink  className="nav-link" to="/">Home <span className="sr-only"></span></NavLink>
-//                         </li>
-//                         <li className="nav-item">
-//                             <NavLink  className="nav-link" to="/about">About</NavLink>
-//                         </li>
-//                         <li className="nav-item">
-//                             <NavLink  className="nav-link" to="/login">Login</NavLink>
-//                         </li>
-//                         <li className="nav-item">
-//                             <NavLink  className="nav-link" to="/contact">Contact</NavLink>
-//                         </li>
-//                         <li className="nav-item">
-//                             <NavLink  className="nav-link" to="/signup">signup</NavLink>
-//                         </li>
-//                     </ul>
-        
-//                 </div>
-//             </nav>
-//        </div>
-//     )
-// }
+const signin = () => {
+    return (
+        <div className="signin container col-md-4">
+            <h1>signin</h1>
+            <div className="container mt-5">
+                <form action="" className="register-form" id="register-form">
 
-// export default Navbar
+                    <div className="form-group mb-3">
+                        <label for="email" className="form-label">Email</label>
+                        <input type="email" className="form-control" name='email' id="email" />
+                    </div>
+
+                    <div className="form-group mb-3">
+                        <label for="password" className="form-label">Password</label>
+                        <input type="password" className="form-control" name='password' id="password" />
+                    </div>
+
+                    <div className="form-group mb-3 form-button">
+                        {/* <label for="cpassword" class="form-label">Confirm Password</label> */}
+                        <input type="submit" className="btn btn-primary" name='signin' id="signin" value="Log In" />
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default signin
