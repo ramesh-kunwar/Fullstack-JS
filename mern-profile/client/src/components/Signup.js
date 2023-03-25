06705e31397ff9fa7b9368d59046825834358268
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2023-03-25 21:41:43 +0545

    Signup - data sent to backend

--- mern-profile/client/src/components/Signup.js
+++ mern-profile/client/src/components/Signup.js
@@ -1,44 +1,104 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
+
+    const navigate = useNavigate()
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        work: "",
+        password: "",
+        cpassword: "",
+    })
+
+
+    const handleInputs = (e) => {
+        let fieldType, fieldValue;
+        console.log(e.target.value);
+        fieldType = e.target.name; // gives the  name of input field
+        fieldValue = e.target.value;
+
+        setUser({ ...user, [fieldType]: fieldValue })
+        console.log(user);
+    }
+
+    const postData = async (e) => {
+        e.preventDefault()
+        const { name, email, phone, work, password, cpassword } = user;
+        const res = await fetch("/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ name, email, phone, work, password, cpassword })
+        })
+        const data = await res.json();
+        if (data.status === 422 || !data) {
+            window.alert("Invalid Registration")
+        } else {
+            window.alert("Registration Successful")
+            navigate("/login")()
+        }
+    }
     return (
         <div className="signup container col-md-4">
             <h1>Signup</h1>
             <div className="container mt-5">
-                <form action="" className="register-form" id="register-form">
+                <form method='POST' className="register-form" id="register-form">
                     <div className="form-group mb-3">
-                        <label for="name" className="form-label">Name</label>
-                        <input type="text" className="form-control" name='name' id="name" />
+                        <label htmlFor="name" className="form-label">Name</label>
+                        <input
+                            value={user.name}
+                            onChange={handleInputs}
+                            type="text" className="form-control" name='name' id="name" />
                     </div>
 
                     <div className="form-group mb-3">
-                        <label for="email" className="form-label">Email</label>
-                        <input type="email" className="form-control" name='email' id="email" />
+                        <label htmlFor="email" className="form-label">Email</label>
+                        <input
+                            value={user.email}
+                            onChange={handleInputs}
+                            type="email" className="form-control" name='email' id="email" />
                     </div>
 
                     <div className="form-group mb-3">
-                        <label for="phone" className="form-label">Phone</label>
-                        <input type="number" className="form-control" name='phone' id="phone" />
+                        <label htmlFor="phone" className="form-label">Phone</label>
+                        <input
+                            value={user.phone}
+                            onChange={handleInputs}
+                            type="number" className="form-control" name='phone' id="phone" />
                     </div>
 
                     <div className="form-group mb-3">
-                        <label for="Work" className="form-label">Profession</label>
-                        <input type="text" className="form-control" name='work' id="work" />
+                        <label htmlFor="Work" className="form-label">Profession</label>
+                        <input
+                            value={user.work}
+                            onChange={handleInputs}
+                            type="text" className="form-control" name='work' id="work" />
                     </div>
 
                     <div className="form-group mb-3">
-                        <label for="password" className="form-label">Password</label>
-                        <input type="password" className="form-control" name='password' id="password" />
+                        <label htmlFor="password" className="form-label">Password</label>
+                        <input
+                            value={user.password}
+                            onChange={handleInputs}
+                            type="password" className="form-control" name='password' id="password" />
                     </div>
 
                     <div className="form-group mb-3">
-                        <label for="cpassword" className="form-label">Confirm Password</label>
-                        <input type="password" className="form-control" name='cpassword' id="cpassword" />
+                        <label htmlFor="cpassword" className="form-label">Confirm Password</label>
+                        <input
+                            value={user.cpassword}
+                            onChange={handleInputs}
+                            type="password" className="form-control" name='cpassword' id="cpassword" />
                     </div>
 
                     <div className="form-group mb-3 form-button">
-                        {/* <label for="cpassword" class="form-label">Confirm Password</label> */}
-                        <input type="submit" className="btn btn-primary" name='signup' id="signup" value="Register" />
+                        <input
+                            onClick={postData}
+                            type="submit" className="btn btn-primary" name='signup' id="signup" value="Register" />
                     </div>
 
                 </form>
