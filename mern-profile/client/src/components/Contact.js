6623f7cbd5d8ceab8edb4220454620a80513ca2a
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2023-03-25 17:16:17 +0545

    Contact and Home page added

--- mern-profile/client/src/components/Contact.js
+++ mern-profile/client/src/components/Contact.js
@@ -2,7 +2,37 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <form className="container mt-5">
+      <div className="row">
+        <div className="col">
+          <h3>Phone</h3>
+          <p>9803468381</p>
+        </div>
+        <div className="col">
+          <h3>Email</h3>
+          <p>rameshkunwar1110@gmail.com</p>
+        </div>
+        <div className="col">
+          <h3>Address</h3>
+          <p>Dhobihat, Lalitpur</p>
+        </div>
+      </div>
+      <h1 className="mt-5">
+        Get In Touch
+      </h1>
+      <div className="row mt-3">
+        <div className="col"><input className='form-control' type="text" placeholder='Your Name' /></div>
+        <div className="col"><input className='form-control' type="email" placeholder='You Email' /></div>
+        <div className="col"><input className='form-control' type="number" placeholder='You Phone Number' /></div>
+        <div className="col mt-5">
+        </div>
+        <div className='container'>
+          <textarea name="" id="" className="form-control my-4" rows="5" placeholder='Your Messege'></textarea>
+
+        </div>
+      </div>
+      <input type="submit" value="Send Message" className='btn btn-primary' />
+    </form>
   )
 }
 
--- mern-profile/client/src/components/Home.js
+++ mern-profile/client/src/components/Home.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="bg-dark text-white text-center  "
+      style={{ height: "90vh", display: "flex", alignItems: "center", justifyContent: "center" }}
+    >
+      <div className="" >
+        <h4 className=' fw-lighter'>HELLO</h4>
+        <h1 className='fs-1 fw-bold'>I Am A MERN Developer</h1>
+      </div>
+    </div>
   )
 }
 
