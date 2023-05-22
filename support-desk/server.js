3fea0dfab11c59a731bb5fbc334898c62387a7f6
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2023-05-22 17:52:08 +0545

    Initial setup

--- support-desk/server.js
+++ support-desk/server.js
@@ -1,14 +1,19 @@
 const express = require("express")
 require("dotenv").config()
 const PORT = process.env.PORT || 8000;
+const userRoutes = require("./backend/routes/userRoutes")
 
 const app = express()
+
 
 app.get("/", (req, res) => {
     res.status(200).json({
         message: "Welcome to the support desk api"
     })
 })
+
+app.use("/api/users", userRoutes)
+
 
 
 
