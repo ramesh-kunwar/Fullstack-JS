f24ace00b5732d6c1a7f4e923e31eb62e106ea14
Ramesh Kunwar <109467139+ramesh-kunwar@users.noreply.github.com>
2023-08-27 06:23:41 +0000

    variables in js

---
+++ 01 JavascriptSeries/01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 555443;
+
+let accountEmail = 'ramesh@gmail.com';
+
+var accountPassword = '12345'
+
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
